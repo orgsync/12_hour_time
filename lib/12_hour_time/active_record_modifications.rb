5a5ff54e4c89584c2dc509e0b0fa3915848efdf2
Ben Ellis <bellis@on-site.com>
2011-11-14 14:17:02 +0100

    Copied contents from 12_hour_time plugin version. Previous contents defined a method that was never actually called by ActiveRecord.

--- lib/12_hour_time/active_record_modifications.rb
+++ lib/12_hour_time/active_record_modifications.rb
@@ -1,28 +1,17 @@
-module TwelveHourTime
-  module AR
-    def self.included(base)
-      base.extend(ClassMethods)
-    end
-
-    module ClassMethods
-      def instantiate_time_object_with_ampm(name, values)
-        if values.last < 0
-          hour_idx = ActionView::Helpers::DateTimeSelector::POSITION[:hour] - 1
-          ampm = values.pop
-          if ampm == ActionView::Helpers::DateTimeSelector::AM and values[hour_idx] == 12
-            values[hour_idx] = 0
-          elsif ampm == ActionView::Helpers::DateTimeSelector::PM and values[hour_idx] != 12
-            values[hour_idx] += 12
-          end
-        end
-        super
+class ActiveRecord::Base
+  def instantiate_time_object_with_ampm(name, values)
+    if values.last < 0
+      hour_idx = ActionView::Helpers::DateTimeSelector::POSITION[:hour] - 1
+      ampm = values.pop
+      if ampm == ActionView::Helpers::DateTimeSelector::AM and values[hour_idx] == 12
+        values[hour_idx] = 0
+      elsif ampm == ActionView::Helpers::DateTimeSelector::PM and values[hour_idx] != 12
+        values[hour_idx] += 12
       end
     end
-  end
-end
 
-if defined? ActiveRecord
-  ActiveRecord::Base.class_eval do
-    include TwelveHourTime::AR
+    instantiate_time_object_without_ampm(name, values)
   end
+
+  alias_method_chain :instantiate_time_object, :ampm
 end
